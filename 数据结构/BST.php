e388e661afa166c858fa7cc274feec937da32e98
artisan <gaoqi198@163.com>
2021-07-23 22:06:06 +0800

    Create BST.php
    构建BST

---
+++ 数据结构/BST.php
@@ -0,0 +1,41 @@
+typedef struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+} Node;
+
+typedef struct tree {
+    struct node *root;
+} Tree;
+
+#build a binary search tree
+function build_bst($tree, $val) {
+    //构建一个val的节点
+    $n = new Node;
+    $n->data = $val;
+    $n->left = null;
+    $n->right = null;
+    
+    if ($tree->root == null) {
+        $tree->root = $n;
+    } else {
+        $temp = $tree->root;
+        while($temp) {
+            if ($val > $temp->data) {
+                //右侧
+                if ($temp->right) {
+                    $temp = $temp->right;                
+                } else {
+                    $temp->right = $n;                
+                }
+            } else {
+                //左侧
+                 if ($temp->left) {
+                     $temp = $temp->left;                 
+                 } else {
+                     $temp->left = $n;                 
+                 }
+            }
+        }
+    }
+}
