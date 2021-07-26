89ef7490253af3edd490b1efe5b74d9a9291329a
artisan <gaoqi198@163.com>
2021-07-26 20:17:51 +0800

    Update 二叉树遍历.php
    中序非递归遍历

--- 数据结构/二叉树遍历.php
+++ 数据结构/二叉树遍历.php
@@ -31,3 +31,24 @@ function preOrder($root) {
     }
     return $result;
 }
+
+//中序非递归遍历
+function inOrder($root) {
+    if (!$root) return [];
+    
+    $stack = [];
+    $result = [];
+    
+    $node = $root;
+    while ($stack || $node) {
+        while($node) {
+            $stack[] = $node;
+            $node = $node->left;
+        }
+        
+        $n = array_pop($stack);
+        $result[] = $n->val;
+        $node = $node->right;
+    }
+    return $result;
+}
