734d684d3e09ee33f52d0ccaca38055e0c0ed340
artisan <gaoqi198@163.com>
2021-07-26 22:56:51 +0800

    Update and rename 二叉树遍历.php to 二叉树先序遍历.php
    update

--- 数据结构/二叉树先序遍历.php
+++ 数据结构/二叉树先序遍历.php
@@ -31,24 +31,3 @@ function preOrder($root) {
     }
     return $result;
 }
-
-//中序非递归遍历
-function inOrder($root) {
-    if (!$root) return [];
-    
-    $stack = [];
-    $result = [];
-    
-    $node = $root;
-    while ($stack || $node) {
-        while($node) {
-            $stack[] = $node;
-            $node = $node->left;
-        }
-        
-        $n = array_pop($stack);
-        $result[] = $n->val;
-        $node = $node->right;
-    }
-    return $result;
-}
