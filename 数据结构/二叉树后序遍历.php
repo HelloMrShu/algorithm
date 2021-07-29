150d9336623b43e2561484a4f42a62521b00ce35
artisan <gaoqi198@163.com>
2021-07-29 23:52:00 +0800

    Update 二叉树后序遍历.php

--- 数据结构/二叉树后序遍历.php
+++ 数据结构/二叉树后序遍历.php
@@ -4,6 +4,25 @@
  * 后序遍历
  */
 
+function postOrder($root)
+{
+    if (!$root) return [];
+    
+    $stack = array();
+    $data = array();
+    array_push($stack, $root);
+    while(!empty($stack)){
+        $center_node = array_pop($stack);
+        array_push($data, $center_node->val);
+        if($center_node->right != null)
+            array_push($stack, $center_node->right);
+        if($center_node->left != null)
+            array_push($stack, $center_node->left);
+    }
+    
+    return array_reverse($data);
+}
+
 function recursivePostOrder($root)
 {
     if (!$root) return [];
